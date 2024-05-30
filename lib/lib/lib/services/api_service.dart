81e2cba8506dfa380811bd525d020e1e921eaf15
Doğukan Atmaca <145132384+dogukanatm@users.noreply.github.com>
2024-05-31 02:50:20 +0300

    Update api_service.dart

--- lib/lib/lib/services/api_service.dart
+++ lib/lib/lib/services/api_service.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/category.dart';
+import 'package:mobile_application_development_course_project/lib/lib/models/category.dart';
 
 class ApiService {
-  static const String apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  static const String apiUrl = 'https://example.com/api/categories';
 
   Future<List<Category>> fetchCategories() async {
     final response = await http.get(Uri.parse(apiUrl));
 
     if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body);
-      return data.map((json) => Category.fromJson(json)).toList();
+      final List<dynamic> responseData = json.decode(response.body);
+      return responseData.map((json) => Category.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load categories');
     }
