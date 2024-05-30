3f57179b1f39c594475cd8fc72240e56dd54cde4
Doğukan Atmaca <145132384+dogukanatm@users.noreply.github.com>
2024-05-31 02:49:52 +0300

    Update category.dart

--- lib/lib/lib/models/category.dart
+++ lib/lib/lib/models/category.dart
@@ -1,13 +1,13 @@
 class Category {
   final int id;
-  final String? name;
+  final String name;
 
-  Category({required this.id, this.name});
+  Category({required this.id, required this.name});
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
       id: json['id'],
-      name: json['name'] as String?,
+      name: json['name'],
     );
   }
 }
